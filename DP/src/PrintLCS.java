3ab7a6e6791f3ea598b2f831a05836dbdd486f43
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-12-30 03:06:53 +0530

    add PrintLCS class with dynamic programming solution for finding the longest common subsequence

---
+++ DP/src/PrintLCS.java
@@ -0,0 +1,41 @@
+class PrintLCS {
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println("Longest Common Subsequence: " + printLCS(text1, text2));
+    }
+
+    public static String printLCS(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        char[] arr1 = text1.toCharArray();
+        char[] arr2 = text2.toCharArray();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (arr1[i - 1] == arr2[j - 1]) {
+                lcs.append(arr1[i - 1]);
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
+    }
+}
