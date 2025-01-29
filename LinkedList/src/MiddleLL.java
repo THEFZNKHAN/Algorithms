2096da8c10e4a2a0064c208e0cc5be99c0d04ae3
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-01-30 03:54:55 +0530

    add MiddleLL class to find the middle element of a linked list and Node class for linked list nodes

---
+++ LinkedList/src/MiddleLL.java
@@ -0,0 +1,24 @@
+public class MiddleLL {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertAtEnd(1);
+        list.insertAtEnd(2);
+        list.insertAtEnd(3);
+        list.insertAtEnd(4);
+        list.insertAtEnd(5);
+
+        System.out.println("Middle element is: " + middle(list.head).data);
+    }
+
+    private static Node middle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
