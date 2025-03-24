a443753cc35a34b1043614ab148c5a4ad2e05306
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-03-24 23:19:41 +0530

    refactor the code

--- COLLEGE LEETCODE/BinarySearch/AggressiveCows.java
+++ COLLEGE LEETCODE/BinarySearch/AggressiveCows.java
@@ -2,47 +2,32 @@ import java.util.*;
 
 class AggressiveCows {
     public static void main(String[] args) {
-        int[] stalls = {1, 2, 4, 8, 9};
-        int k = 3;
-        System.out.println(aggressiveCows(stalls, k));
+        System.out.println(aggressiveCows(new int[]{1, 2, 4, 8, 9}, 3));
         System.out.println(aggressiveCows(new int[]{10, 1, 2, 7, 5}, 3));
         System.out.println(aggressiveCows(new int[]{2, 12, 11, 3, 26, 7}, 5));
     }
+
     public static int aggressiveCows(int[] stalls, int k) {
         Arrays.sort(stalls);
-        int max = (stalls[stalls.length - 1] - stalls[0]) / (k - 1), ans = 0, s = 0;
+        int n = stalls.length;
+        int max = (stalls[stalls.length - 1] - stalls[0]) / (k - 1), s = 0;
         while (s <= max) {
             int m = s + (max - s) / 2;
-            if (helper(stalls, m, k)) {
-                ans = m;
-                s = m + 1;
-            } else {
-                max = m - 1;
-            }
+            if (helper(stalls, m, k, n)) s = m + 1;
+            else max = m - 1;
         }
-        return ans;
+        return max;
     }
 
-    // public static int aggressiveCows(int[] stalls, int k) {
-    //     Arrays.sort(stalls);
-    //     int max = stalls[stalls.length - 1], ans = 0;
-    //     for (int i = max; i > 0; i--) {
-    //         if (helper(stalls, i, k)) {
-    //             ans = i;
-    //             break;
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-    private static boolean helper(int[] stalls,int d, int k) {
-        int ctr = 1, last = stalls[0], n = stalls.length;
+    private static boolean helper(int[] stalls,int d, int k, int n) {
+        int ctr = 1, last = stalls[0];
         for (int i = 1; i < n; i++) {
             if (stalls[i] - last >= d) {
                 ctr++;
                 last = stalls[i];
             }
+            if (ctr >= k) return true;
         }
-        return ctr >= k;
+        return false;
     }
-} 
+}
--- COLLEGE LEETCODE/SlidingWindow/MaximumSumRectangle.java
+++ COLLEGE LEETCODE/SlidingWindow/MaximumSumRectangle.java
@@ -1,11 +1,6 @@
 class MaximumSumRectangle {
     public static void main(String[] args) {
-        int[][] mat = {
-            {1, 2, -1, -4, -20},
-            {-8, -3, 4, 2, 1},
-            {3, 8, 10, 1, 3},
-            {-4, -1, 1, 7, -6}
-        };
+        int[][] mat = { {1, 2, -1, -4, -20}, {-8, -3, 4, 2, 1}, {3, 8, 10, 1, 3}, {-4, -1, 1, 7, -6} };
         int result = maximumSumRectangle(mat);
         System.out.println("Maximum Sum Rectangle: " + result);
     }
