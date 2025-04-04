7a8ebe5edbe129b791b87f8d3f0f6d9f95ce7b1a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-04-04 21:08:45 +0530

    add KthElementTwoArrays class with implementation to find the kth element in two sorted arrays

---
+++ COLLEGE LEETCODE/BinarySearch/KthElementTwoArrays.java
@@ -0,0 +1,31 @@
+public class KthElementTwoArrays {
+    public static void main(String[] args) {
+        int[] a = {2, 3, 6, 7, 9};
+        int[] b = {1, 4, 8, 10};
+        int k = 5;
+
+        int result = kthElement(a, b, k);
+        System.out.println("The " + k + "th element is: " + result);
+    }
+
+    public static int kthElement(int a[], int b[], int left) {
+        int n1 = a.length, n2 = b.length;
+        if (n1 > n2) return kthElement(b, a, left);
+
+        int low = Math.max(0, left - n2), high = Math.min(left, n1);
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) return Math.max(l1, l2);
+            else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0;
+    }
+}
