5c83127c1e911cb0047a9bea1bfb05572c52138f
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-04-23 21:31:27 +0530

    add space optimization method to Knapsack class and update main method to display its result

--- COLLEGE LEETCODE/dp/Knapsack.java
+++ COLLEGE LEETCODE/dp/Knapsack.java
@@ -6,12 +6,28 @@ class Knapsack {
         int maxProfit = knapsack(W, val, wt);
         System.out.println("Maximum profit: " + maxProfit);
         System.out.println("Maximum profit: " + tab(W, val, wt));
+        System.out.println("Maximum profit: " + space_opt(W, val, wt));
+    }
+
+    // SPACE OPTIMIZED
+    private static int space_opt(int W, int[] val, int[] wt) {
+        int n = val.length;
+        int[] prev = new int[W + 1];
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[W + 1];
+            for (int j = 1; j <= W; j++) {
+                if (wt[i - 1] <= j) curr[j] = val[i - 1] + prev[j - wt[i- 1]];
+                curr[j] = Math.max(curr[j], prev[j]);
+            }
+            prev = curr.clone();
+        }
+        return prev[W];
     }
 
     // TABULATION
     private static int tab(int W, int[] val, int[] wt) {
         int n = val.length;
-        int[][] dp = new int[n + 1][ W + 1];
+        int[][] dp = new int[n + 1][W + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= W; j++) {
                 if (wt[i - 1] <= j) dp[i][j] = val[i - 1] + dp[i - 1][j - wt[i- 1]];
