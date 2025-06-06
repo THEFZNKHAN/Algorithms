57bf4c37a75ecab726fcd9068b29adc781cffa26
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2025-06-07 01:56:16 +0530

    enhance Trie class with counter functionality for word occurrences and prefixes

--- COLLEGE LEETCODE/Tree/Trie/Trie.java
+++ COLLEGE LEETCODE/Tree/Trie/Trie.java
@@ -2,10 +2,14 @@ class Trie {
     class TrieNode {
         TrieNode[] child;
         boolean isEnd;
+        int cntPrefix;
+        int ctr;
 
         public TrieNode() {
-            child = new TrieNode[26];
-            isEnd = false;
+            this.child = new TrieNode[26];
+            this.isEnd = false;
+            this.cntPrefix = 0;
+            this.ctr = 0;
         }
 
         public boolean containsKey(char c) {
@@ -29,7 +33,31 @@ class Trie {
         }
 
         public boolean isEnd() {
-            return isEnd;
+            return this.isEnd;
+        }
+
+        public void incrementCtr() {
+            this.ctr++;
+        }
+
+        public void decrementCtr() {
+            this.ctr--;
+        }
+        
+        public int getCounter() {
+            return ctr;
+        }
+        
+        public void incrementPC() {
+            this.cntPrefix++;
+        }
+
+        public void decrementPC() {
+            this.cntPrefix--;
+        }
+
+        public int getPrefixCounter() {
+            return cntPrefix;
         }
     }
 
@@ -44,8 +72,10 @@ class Trie {
         for (char c : word.toCharArray()) {
             if (!temp.containsKey(c)) temp.put(c);
             temp.get(c); // temp = temp.child[c];
+            temp.incrementPC();
         }
         temp.setEnd();
+        temp.incrementCtr();
     }
 
     public boolean search(String word) {
@@ -64,5 +94,33 @@ class Trie {
             temp.get(c);
         }
         return true;
+    }
+
+    public int countWordEqualTo(String word) {
+        TrieNode temp = root;
+        for (char c : word.toCharArray()) {
+            if (!temp.containsKey(c)) return 0;
+            temp.get(c);
+        }
+        return temp.getCounter();
+    }
+
+    public int countWordStartingWith(String word) {
+        TrieNode temp = root;
+        for (char c : word.toCharArray()) {
+            if (!temp.containsKey(c)) return 0;
+            temp.get(c);
+        }
+        return temp.getPrefixCounter();
+    }
+
+    public void erase(String word) {
+        TrieNode temp = root;
+        for (char c : word.toCharArray()) {
+            if (!temp.containsKey(c)) return;
+            temp.decrementPC();
+            temp.get(c);
+        }
+        temp.decrementCtr();
     }
 }
